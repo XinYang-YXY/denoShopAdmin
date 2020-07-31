eba2a4ace13ca114576e51cc28145780031153f5
XJLim <limxiujia18@gmail.com>
2020-08-01 01:05:39 +0800

    Updated Order Model

--- models/Order.js
+++ models/Order.js
@@ -5,12 +5,24 @@ const Order = db.define("order", {
     chargeId: {
         type: Sequelize.STRING,
     },
+    paypalId: {
+        type: Sequelize.STRING,
+    },
     deliveryDate: {
         type: Sequelize.DATEONLY,
     },
     deliveryTime: {
         type: Sequelize.STRING,
     },
+    orderDescription: {
+        type: Sequelize.STRING,
+    },
+    orderStatus: {
+        type: Sequelize.INTEGER,
+    },
+    orderSum: {
+        type: Sequelize.FLOAT,
+    }
 })
 
 module.exports = Order;
